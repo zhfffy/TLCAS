8c849a7e0e91d00ad8fcb4129d205fa1ce6d74dc
zhfffy <564952043@qq.com>
2020-10-19 21:49:48 +0800

    111

--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def analysis(request): #request是必须带的实例。类似class下方法必须带self一样, 
+    message = '感谢确认，请使用账户登录！'
+    return render(request, 'analysis/analysis.html', {'message': message})
+    #return HttpResponse("Hello World")#通过HttpResponse模块直接返回字符串到前端页面
